45708e8a3bb75d076629a7a71b0ef7c643b881f2
Soumyajit <4soumya19@gmail.com>
2024-09-22 18:53:55 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -5,19 +5,40 @@ const express = require("express");
 const app = express();
 
 //request handler
+// order is very much imp---Order matters
 
-app.use("/",(req,res) =>{
-    res.send("Hello from dashboard");
+app.use("/user",(req,res) =>{
+  res.send("HAAAA");
 })
 
-app.use("/test",(req,res) =>{
-    res.send("Hello from server");
+//This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Soumya", lastName: "Saini" });
+});
+
+app.post("/user", (req, res) => {
+  // console.log("Save Data to the Database");
+  //saving the data to DB
+  res.send("Data successfully saved to the database");
+});
+
+app.delete("/user", (req,res) =>{
+  res.send("Deleted");
 })
 
-app.use("/hello",(req,res) =>{
-    res.send("Hello from server");
-})
+// this will match all the http method API calls to /test
+app.use("/test", (req, res) => {
+  res.send("Hello from server");
+});
 
-app.listen(3000,()=>{
-    console.log("Server is started 3000");
+// app.use("/hello",(req,res) =>{
+//     res.send("Hello from server");
+// })
+
+// app.use("/",(req,res) =>{
+//     res.send("Hello from dashboard");
+// })
+
+app.listen(3000, () => {
+  console.log("Server is started 3000");
 });
